757e8408fb50fb7c609ae8c90ffbc674f4a9631f
Hidetaka Matsuda <sopmod120@gmail.com>
2025-02-23 08:51:27 +0900

    プルリクテスト

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.use("/campgrounds", campgroundsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404, "ページが見つかりません"));
+  next(createError(404, "ページが見つかりませ"));
 });
 
 // error handler
